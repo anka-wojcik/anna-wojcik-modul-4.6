66e0a3fac832364107ff73203dbc6a76a5c0ec5c
“anka-wojcik <anka.wojcik@yahoo.com>
2018-03-05 11:18:52 +0100

    First Commit

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.kalkulator;
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractBFromA(int a, int b) {
+        return a - b;
+    }
+}
